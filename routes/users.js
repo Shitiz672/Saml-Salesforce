6c9eac1d230037c6071b8daa55cf42754c51032a
Shitiz672 <73930608+Shitiz672@users.noreply.github.com>
2020-11-08 00:32:00 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -12,18 +12,13 @@ console.log("Users %d",8);
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
-  console.log(JSON.parse(req) +" HAHHAH")
+  
   
   fs.appendFile('userInfo.txt', JSON.stringify(req.user)+"\r\n", function (err) {
   if (err) throw err;
   console.log('Saved!');
   
-request('http://boxinallsoftech.com/SSOLogin/WriteFile.php?data='+JSON.stringify(req.user), function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-      console.log("success");
-    //console.log(body) // Show the HTML for the Google homepage. 
-  }
-});
+
   
   
 }); 
@@ -34,27 +29,6 @@ request('http://boxinallsoftech.com/SSOLogin/WriteFile.php?data='+JSON.stringify
   });
 });
 
-/* GET the profile of the current authenticated user */
-router.get('/profile', function(req, res, next) {
-  request.get(
-    `https://bia-dev.onelogin.com/oidc/2/me`,   
-    {
-    'auth': {
-      'bearer': req.session.accessToken
-    }
-  },function(err, respose, body)
-  {
 
-    console.log('User FF')
-	
-    console.log(req);
-
-    res.render('profile', {
-      title: 'Profile',
-      user: JSON.parse(req)
-    });
-
-  });
-});
 
 module.exports = router;
